73e851568acd0fa14347bf9b122984272327dc68
Paul <paul@liquidlogic.eu>
2021-09-03 10:31:53 +0200

    Showing Blockly

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useBlocklyWorkspace } from 'react-blockly';
+import { useRef } from 'react'
 
 function App() {
+
+  const toolboxCategories = {
+    kind: "categoryToolbox",
+    contents: [
+      {
+        kind: "category",
+        name: "Logic",
+        colour: "#5C81A6",
+        contents: [
+          {
+            kind: "block",
+            type: "controls_if",
+          },
+          {
+            kind: "block",
+            type: "logic_compare",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "Math",
+        colour: "#5CA65C",
+        contents: [
+          {
+            kind: "block",
+            type: "math_round",
+          },
+          {
+            kind: "block",
+            type: "math_number",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "Custom",
+        colour: "#5CA699",
+        contents: [
+          {
+            kind: "block",
+            type: "new_boundary_function",
+          },
+          {
+            kind: "block",
+            type: "return",
+          },
+        ],
+      },
+    ],
+  };    const initialXml =
+    '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
+
+  const blocklyRef = useRef(null);
+  const { workspace, xml} = useBlocklyWorkspace({
+    ref: blocklyRef,
+    toolboxConfiguration: toolboxCategories, // this must be a JSON toolbox definition
+    initialXml: initialXml,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={blocklyRef} style={{height: '1000px'}} />
     </div>
-  );
+    
+  )
 }
 
 export default App;
