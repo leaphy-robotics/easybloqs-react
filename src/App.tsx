3695c98d0df58ccd2566be10b7cb97470e091556
Paul <paul@liquidlogic.eu>
2021-09-04 10:57:19 +0200

    Including leaphy-blockly media assets manually for now

--- src/App.tsx
+++ src/App.tsx
@@ -6,54 +6,36 @@ function App() {
 
   const toolboxCategories = {
     kind: "categoryToolbox",
+    id:"easyBloqsToolbox",
     contents: [
       {
         kind: "category",
-        name: "Logic",
-        colour: "#5C81A6",
+        name: "%{BKY_LEAPHY_SITUATION_CATEGORY}",
+        id: "l_situation",
+        toolboxitemid: "l_situation",
+        categorystyle: "situation_category",
         contents: [
           {
             kind: "block",
-            type: "controls_if",
+            type: "time_delay",
+            blockxml: `<block type="time_delay">
+            <value name="DELAY_TIME_MILI">
+              <shadow type="math_number">
+                <field name="NUM">1000</field>
+              </shadow>
+            </value>
+          </block>`
           },
           {
             kind: "block",
             type: "logic_compare",
           },
         ],
-      },
-      {
-        kind: "category",
-        name: "Math",
-        colour: "#5CA65C",
-        contents: [
-          {
-            kind: "block",
-            type: "math_round",
-          },
-          {
-            kind: "block",
-            type: "math_number",
-          },
-        ],
-      },
-      {
-        kind: "category",
-        name: "Custom",
-        colour: "#5CA699",
-        contents: [
-          {
-            kind: "block",
-            type: "new_boundary_function",
-          },
-          {
-            kind: "block",
-            type: "return",
-          },
-        ],
-      },
+      }
     ],
-  };    const initialXml =
+  };    
+  
+  const initialXml =
     '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
 
   const blocklyRef = useRef(null);
